17652f8ce9e42f7c6c8e7f8e48b5ea33fe77e7a9
dwp <894074731@qq.com>
2017-12-01 10:09:25 +0800

    添加文件

---
+++ lie/prefect.js
@@ -0,0 +1,10 @@
+
+var app = new Vue({
+	el:"#app",
+	data:{
+		text:"hello world!",
+		name:null,
+		age:null,
+		sex:null
+	}
+})
---
+++ lie/v-for.js
@@ -0,0 +1,21 @@
+var app = new Vue({
+	el:"#app",
+	data:{
+		foodList:[
+			{
+				name:"葱",
+				price: 25,
+				discount:0.8
+			},
+			{
+				name:"姜",
+				price: 20,
+				discount:0.8
+			},
+			{
+				name:"蒜",
+				price: 18
+			}
+		]
+	}
+})
